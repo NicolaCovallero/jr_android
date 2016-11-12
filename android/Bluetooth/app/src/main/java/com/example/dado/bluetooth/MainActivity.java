7ad9c10b7bf3dcbd2177a9d23f1f51f508a7ad77
NicolaCovallero <nicolacovallero92@gmail.com>
2016-11-12 10:16:13 +0100

    git test

--- android/Bluetooth/app/src/main/java/com/example/dado/bluetooth/MainActivity.java
+++ android/Bluetooth/app/src/main/java/com/example/dado/bluetooth/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.dado.bluetooth;
-// comment to test git
+// comment to test gita
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
